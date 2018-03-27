92fce49466dd1538b708f13e8aa6c3a8376ff3cc
LetiTran <leticiastran@gmail.com>
2018-03-27 14:50:13 -0700

    Comlpeted the Hamming#compute method and passed all the tests. Also unskipped tests and added 'minitest/reporters' to the spec file for better view.

--- 03-hamming/hamming.rb
+++ 03-hamming/hamming.rb
@@ -1,7 +1,15 @@
 class Hamming
 
-  def self.compute(first, second)
-    raise ArgumentError if first.length != second.length
+  def self.compute(first_strand, second_strand)
+    raise ArgumentError if first_strand.length != second_strand.length
+
+    first = first_strand.split("")
+    second = second_strand.split("")
+    hamming_distance = 0
+
+    first.size.times {|i| hamming_distance += 1 if first[i] != second[i]}
+
+    return hamming_distance
   end
 
 end
