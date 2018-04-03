71328b99e9f37b6e25e3cc06b466fd1fe5a4645d
LetiTran <leticiastran@gmail.com>
2018-04-03 09:34:10 -0700

    Deleted the .split part to make it more effient.

--- 03-hamming/hamming.rb
+++ 03-hamming/hamming.rb
@@ -3,11 +3,9 @@ class Hamming
   def self.compute(first_strand, second_strand)
     raise ArgumentError if first_strand.length != second_strand.length
 
-    first = first_strand.split("")
-    second = second_strand.split("")
     hamming_distance = 0
 
-    first.size.times {|i| hamming_distance += 1 if first[i] != second[i]}
+    first_strand.size.times {|i| hamming_distance += 1 if first_strand[i] != second_strand[i]}
 
     return hamming_distance
   end
